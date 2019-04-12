625e8348a6c3eabbc2a7650c39f5011368a116ee
rmayer <cassettes90@gmail.com>
2019-04-12 00:06:51 -0300

    nuevos cambios

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write(`<h1>Hello I'm Maxi!</h1>`);
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
--- server-local.js
+++ server-local.js
@@ -1,5 +1,6 @@
 'use strict';
 
 const app = require('./express/server');
-
+console.log('entres');
+console.log(app);
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
