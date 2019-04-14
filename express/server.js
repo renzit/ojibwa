9c0007c816f19c4d752b76b163887e3cbdd90baa
rmayer <cassettes90@gmail.com>
2019-04-14 00:24:28 -0300

    first html , add nav

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Maximiliano Echenique Fotógrafo</h1>`);
+  res.write(`<h1>Maximiliano Echenique</h1>`);
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
