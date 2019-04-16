d8ae302209cb4218d208b9464d132c79545d5bed
rmayer <cassettes90@gmail.com>
2019-04-15 23:21:44 -0300

    try updates with arquitecture

--- express/server.js
+++ express/server.js
@@ -1,20 +1,41 @@
+// 'use strict';
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+// const router = express.Router();
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write(`<h1>Maximiliano Echenique</h1>`);
+//   res.end();
+// });
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
 'use strict';
 const express = require('express');
-const serverless = require('serverless-http');
+// const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Maximiliano Echenique</h1>`);
-  res.end();
+  res.sendfile(__dirname+'/public/index.html');
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use(bodyParser.json());
+app.use(express.static('public'), router)
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
--- server-local.js
+++ server-local.js
@@ -1,4 +1,13 @@
-'use strict';
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const app = require('./express/server');
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+// This serves static files from the specified directory
+app.use(express.static(path.join(__dirname, 'public')));
+const server = app.listen(8081, () => {
+
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
