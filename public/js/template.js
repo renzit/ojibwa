8e64dea0baf3b1a489666adbf7736aad46c92c21
rmayer <cassettes90@gmail.com>
2019-05-05 02:23:28 -0300

    add masonry

--- public/js/template.js
+++ public/js/template.js
@@ -11,7 +11,9 @@ function addItems(itemList){
         element.src = `data:image/svg+xml,%3csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${item.width} ${item.height}"%3e%3crect fill="%23F8F8F8" width="100%25" height="100%25"/%3e%3c/svg%3e`;
         element.setAttribute('data-src', `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.webp` );
         grid.appendChild(node);
+        console.log('agregando items');
     });
+    console.log('termine con los items');
 }
 
 function calculateAspectRatio(width, height){
