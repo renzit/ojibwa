65460f1704594cf12048d5a3800c4db44f2366b3
rmayer <cassettes90@gmail.com>
2019-05-05 20:15:13 -0300

    final version with lazy loading

--- public/js/template.js
+++ public/js/template.js
@@ -8,9 +8,9 @@ function addItems(itemList) {
         aspectRatio = item.width / itemAspectRatio + ":" + item.height / itemAspectRatio;
         const node = document.importNode(template.content, true);
         const element = node.querySelector('img');
-        //element.src = `data:image/svg+xml,%3csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${item.width} ${item.height}"%3e%3crect fill="%23F8F8F8" width="100%25" height="100%25"/%3e%3c/svg%3e`;
-        element.src = `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.jpg`;
-        //element.setAttribute('data-src', `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.webp`);
+        element.src = `data:image/svg+xml,%3csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${item.width} ${item.height}"%3e%3crect fill="%23F8F8F8" width="100%25" height="100%25"/%3e%3c/svg%3e`;
+        //element.src = `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.jpg`;
+        element.setAttribute('data-src', `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.webp`);
         grid.appendChild(node);
     });
     var msnry;
@@ -21,6 +21,10 @@ function addItems(itemList) {
             columnWidth: '.grid-sizer',
             percentPosition: true
         });
+    });
+    var lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy"
+        // ... more custom settings?
     });
     // const templateScripts = document.querySelector('#template-scripts');
     // const scripts = document.importNode(templateScripts.content, true);
