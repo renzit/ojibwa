09e81cfecaaac046ee2fa6aaa1a133ba3c19ed54
rmayer <cassettes90@gmail.com>
2019-05-05 02:17:29 -0300

    update with previews

--- public/js/template.js
+++ public/js/template.js
@@ -8,7 +8,8 @@ function addItems(itemList){
         aspectRatio = item.width/itemAspectRatio + ":" + item.height/itemAspectRatio;
         const node = document.importNode(template.content, true);
         const element = node.querySelector('img');
-        element.src = `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.webp`;
+        element.src = `data:image/svg+xml,%3csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${item.width} ${item.height}"%3e%3crect fill="%23F8F8F8" width="100%25" height="100%25"/%3e%3c/svg%3e`;
+        element.setAttribute('data-src', `https://res.cloudinary.com/renzit/image/upload/w_1000,ar_${aspectRatio},c_fill,g_auto,e_sharpen/${item.public_id}.webp` );
         grid.appendChild(node);
     });
 }
