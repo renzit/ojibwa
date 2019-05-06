c397cf5f1ccf387d3864a7a7424244fa267aeacc
rmayer <cassettes90@gmail.com>
2019-05-06 01:15:37 -0300

    last updates for mobile sub menu

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -1,6 +1,25 @@
 const subnav = document.querySelector(".subnav");
 subnav.addEventListener("click", function(event){
-    console.log('hola');
     const path = event.target.dataset.collection
     getResourceList(path);
+    toggleSubNav();
 });
+
+const mainNav = document.querySelector(".main-nav");
+mainNav.addEventListener("click", function(event){
+    if(event.target.classList.contains('has-dropdown')){
+        toggleSubNav();
+        return;
+    }else{
+        mainNav.classList.toggle('opened');
+
+    }
+});
+
+function toggleSubNav(){
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if(w < 910){
+        subnav.classList.toggle('show');
+    }
+    console.log(w);
+}
--- public/js/menu.js
+++
@@ -1,7 +0,0 @@
-let burger = document.getElementById('burger'),
-	 nav    = document.getElementById('main-nav');
-
-burger.addEventListener('click', function(e){
-	this.classList.toggle('is-open');
-	nav.classList.toggle('is-open');
-});
