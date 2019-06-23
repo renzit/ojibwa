37297fc8d44e0dba4a5fa4cf8a49e2d6db517b64
Renzo <renzo.mayer@vix.com>
2019-06-23 18:23:21 -0300

    fixes + api + js

--- public/theme/js/grid-items.js
+++ public/theme/js/grid-items.js
@@ -2,20 +2,15 @@
 
 function addItems(itemList) {
     const templatePicture = document.querySelector('#item-template-picture');
-    const grid = document.querySelector('.grid');
-    const div = document.createElement('div');
-    div.setAttribute('class', 'grid-sizer');
+    const grid = document.querySelector('.grid-container');
+    // const div = document.createElement('div');
+    // div.setAttribute('class', 'grid-sizer');
     grid.innerHTML = "";
-    grid.appendChild(div);
+    //grid.appendChild(div);
     itemList.forEach(item => {
         aspectRatioLabel = getAspectRatioLabel(item.width, item.height);
         imgPlaceholder = `data:image/svg+xml,%3csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${item.width} ${item.height}"%3e%3crect fill="%23F8F8F8" width="100%25" height="100%25"/%3e%3c/svg%3e`;
         const node = document.importNode(templatePicture.content, true);
-        const source = node.querySelector('source');
-        source.setAttribute('data-srcset', 
-        `https://res.cloudinary.com/renzit/image/upload/w_400,q_auto,ar_${aspectRatioLabel},c_fill,g_auto,e_sharpen/${item.public_id}.webp 400w,
-        https://res.cloudinary.com/renzit/image/upload/w_800,q_auto,ar_${aspectRatioLabel},c_fill,g_auto,e_sharpen/${item.public_id}.webp 800w`);
-
         const image = node.querySelector('img');
         image.src = imgPlaceholder;
         image.setAttribute('data-src', `https://res.cloudinary.com/renzit/image/upload/w_400,q_auto,ar_${aspectRatioLabel},c_fill,g_auto,e_sharpen/${item.public_id}.jpg`);
@@ -25,15 +20,19 @@ function addItems(itemList) {
 
         grid.appendChild(node);
     });
-
-    var msnry;
-    imagesLoaded(grid, function () {
-        msnry = new Masonry(grid, {
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-sizer',
-            percentPosition: true
-        });
+    jQuery(".img-hover-wrap").hover3d({
+        selector: ".img-hover-card",
+        shine: false,
     });
+
+    // var msnry;
+    // imagesLoaded(grid, function () {
+    //     msnry = new Masonry(grid, {
+    //         itemSelector: '.grid-item',
+    //         columnWidth: '.grid-sizer',
+    //         percentPosition: true
+    //     });
+    // });
 
     var lazyLoadInstance = new LazyLoad({
         elements_selector: ".lazy"
